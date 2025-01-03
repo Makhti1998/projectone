444520ae95cd35364238bd42df463500ab449876
Makhti <msayfidarow@gmail.com>
2025-01-03 21:14:09 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+"use strict";
+
+console.log("arr" + " - object");
+console.log(4 + +"5");
+
+let incr = 10,
+  decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5 % 2);
+
+console.log(2 * 4 == "8");
+
+const isChecked = true,
+  isClose = true;
+
+console.log(isChecked && isClose);
+
+let a = 5;
+let b = 10;
+console.log("Fifteen is " + (a + b) + " and not " + (2 * a + b) + ".");
+
+const c = 5;
+const d = 10;
+console.log(`Fifteen is ${c + d} and not ${2 * c + d}.`);
